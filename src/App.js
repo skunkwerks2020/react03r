f47ca4d7f861340a25967ffcbd7b1ec557ea21a0
SkunkWerks2020 <skunkwerks2020@gmail.com>
2020-07-07 23:28:45 -0600

    edit App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p> This is React03rrrr</p>
+        <p> This is React03r Yes it Works</p>
         <a
           className="App-link"
           href="https://reactjs.org"
